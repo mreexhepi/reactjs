e359c7d8dc37faf3200d6d8907d9ce0bf0e6acf5
Muhamet Rexhepi <Muhamet.Rexhepi@ex-group.local>
2021-12-01 20:36:43 +0100

    Customer services functions added.

--- src/services/Customers.services.js
+++ src/services/Customers.services.js
@@ -1,1 +1,16 @@
-//Customer services
+import http from "../http-common/http-common";
+
+export class CustomersServices{
+    getCustomers(){
+        return http.get("/customers");
+    }
+    static addCustomer(customer){
+       return http.post("/customers", customer);
+    }
+
+    deleteCustomer(id){
+        return http.delete("/customers" +id);
+    }
+}
+
+export default new CustomersServices();
