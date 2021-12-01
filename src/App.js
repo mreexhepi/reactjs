1d99cf12f1459f85f75e1287494f2c23abdf5dea
Muhamet Rexhepi <Muhamet.Rexhepi@ex-group.local>
2021-12-01 19:52:36 +0100

    AddCostumersComponent was added.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import CustomersComponent from "./components/Customers.component";
 import HeaderComponent from "./components/Header.component";
 import FooterComponent from "./components/Footer.component";
-import ProductComponent from "./components/Product.components"
+import ProductComponent from "./components/Product.components";
+import AddCostumersComponent from "./components/AddCostumers.component";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
                       <Route exact path="/" component={CustomersComponent}/>
                       <Route exact path="/customers" component={CustomersComponent}/>
                       <Route exact path="/products" component={ProductComponent}/>
+                      <Route exact path="/add-customer" component={AddCostumersComponent}/>
 
                   </Switch>
                 </div>
